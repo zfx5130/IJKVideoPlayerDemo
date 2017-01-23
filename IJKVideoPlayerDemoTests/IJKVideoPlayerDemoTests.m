6ab1aacc54593c6ea1dc99c76df2aa6bceb0acb5
thomas <dui1cuo@126.com>
2017-01-23 10:41:33 +0800

    [Mod]add demo

---
+++ IJKVideoPlayerDemoTests/IJKVideoPlayerDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  IJKVideoPlayerDemoTests.m
+//  IJKVideoPlayerDemoTests
+//
+//  Created by admin on 17/1/17.
+//  Copyright © 2017年 thomas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IJKVideoPlayerDemoTests : XCTestCase
+
+@end
+
+@implementation IJKVideoPlayerDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
